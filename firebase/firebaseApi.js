b9fa47163a4a7f100c70e6f8e7e129583ab5f8c4
Xinchen <xinchen.zhu94@gmail.com>
2020-04-01 13:12:02 +0800

    added firebase svc encoding

--- firebase/firebaseApi.js
+++ firebase/firebaseApi.js
@@ -1,6 +1,10 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("../firebase-key.json");
+var serviceAccountDec = Buffer.from(
+	process.env.FIREBASE_SVC,
+	"base64"
+).toString("ascii");
+var serviceAccount = JSON.parse(serviceAccountDec);
 
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount)
