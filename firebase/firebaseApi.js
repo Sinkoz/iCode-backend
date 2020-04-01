6678b3fbf92c1fd4663982e001b5dd72d4b2fa0d
Xinchen <xinchen.zhu94@gmail.com>
2020-04-01 09:53:05 +0800

    Added env var for firebase

--- firebase/firebaseApi.js
+++ firebase/firebaseApi.js
@@ -1,9 +1,21 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("../firebase-key.json");
+var firebaseConfig = {
+	type: "service_account",
+	project_id: "storage-ce335",
+	private_key_id: process.env.FIREBASE_PKEY_ID,
+	private_key: process.env.FIREBASE_PKEY,
+	client_email: process.env.FIREBASE_CLIENT_EMAIL,
+	client_id: process.env.FIREBASE_CLIENT_ID,
+	auth_uri: "https://accounts.google.com/o/oauth2/auth",
+	token_uri: "https://oauth2.googleapis.com/token",
+	auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+	client_x509_cert_url:
+		"https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-1cy6f%40storage-ce335.iam.gserviceaccount.com"
+};
 
 admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount)
+	credential: admin.credential.cert(firebaseConfig)
 });
 
 var firebaseDB = admin.firestore();
